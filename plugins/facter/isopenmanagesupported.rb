875dd2f90ee991346d02ceb9d8aa9d801b3cd55e
Marc Fournier <marc.fournier@camptocamp.com>
2008-08-28 23:18:06 +0200

    fix logic: quand on laisse une petite imprécision dans un fact, tout puppet part en vrille ! Pffff... pas gratuit ce ruby...

--- plugins/facter/isopenmanagesupported.rb
+++ plugins/facter/isopenmanagesupported.rb
@@ -21,21 +21,24 @@
 
 supported = ["0x01b1", "0x01b2", "0x01b3", "0x01b6", "0x01b7", "0x01b8", "0x01bb", "0x01e6", "0x01e7", "0x01ea", "0x01f0", "0x010a", "0x0106", "0x0109", "0x011b", "0x0121", "0x0123", "0x0124", "0x0134", "0x0135", "0x014a", "0x0141", "0x016c", "0x016d", "0x016e", "0x016f", "0x0165", "0x0167", "0x0170", "0x018a", "0x0183", "0x0185", "0x020c", "0x020f", "0x0205", "0x0208", "0x0210", "0x023c", "0x023f", "0x0240", "0x027c", "0x0280"]
 
-
 Facter.add("isopenmanagesupported") do
+    setcode do
 
-    if FileTest.exists?("/usr/sbin/getSystemId")
-        setcode do
-            output = `/usr/sbin/getSystemId`
-            systemid = /^System ID:\s+(.*)$/.match(output)[1]
+        if FileTest.exists?("/usr/sbin/getSystemId")
+            output = %x{/usr/sbin/getSystemId}
+            systemid = /^System ID:\s+(.*)$/.match(output)
 
-            if output =~ /^Is Dell:\s+1$/ and supported.include?(systemid.downcase)
+            if systemid.nil? or systemid.length != 2
+                "no"
+            elsif output =~ /^Is Dell:\s+1$/ and supported.include?(systemid[1].downcase)
                 "yes"
             else
                 "no"
             end
+        else
+            nil
         end
-    else
-        nil
+
     end
 end
+
