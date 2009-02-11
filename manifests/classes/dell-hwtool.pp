4b75000ac426d78e21e58d26636929435217bb69
Marc Fournier <marc.fournier@camptocamp.com>
2009-02-11 15:09:07 +0100

    indentation fix

--- manifests/classes/dell-hwtool.pp
+++ manifests/classes/dell-hwtool.pp
@@ -1,37 +1,37 @@
 class dell::hwtools {
 
-    # Dans ces paquets, on trouve de quoi flasher et extraires des infos des
-    # bios & firmwares.
-    package{["libsmbios", "smbios-utils", "firmware-tools"]:
-        ensure => latest,
-        require => Yumrepo["dell-omsa-indep"],
-    }
+  # Dans ces paquets, on trouve de quoi flasher et extraires des infos des
+  # bios & firmwares.
+  package{["libsmbios", "smbios-utils", "firmware-tools"]:
+    ensure => latest,
+    require => Yumrepo["dell-omsa-indep"],
+  }
 
-    file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-dell":
-        ensure => present,
-        source => "puppet:///dell/etc/pki/rpm-gpg/RPM-GPG-KEY-dell",
-        mode => 644,
-    }
+  file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-dell":
+    ensure => present,
+    source => "puppet:///dell/etc/pki/rpm-gpg/RPM-GPG-KEY-dell",
+    mode => 644,
+  }
 
-    file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios":
-        ensure => present,
-        source => "puppet:///dell/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
-        mode => 644,
-    }
+  file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios":
+    ensure => present,
+    source => "puppet:///dell/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
+    mode => 644,
+  }
 
-    # http://linux.dell.com/wiki/index.php/Repository/software
-    yumrepo {"dell-omsa-indep":
-        descr => "Dell OMSA repository - Hardware independent",
-        mirrorlist => "http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el\$releasever&basearch=\$basearch&dellsysidpluginver=\$dellsysidpluginver",
-        enabled => 1,
-        gpgcheck => 1,
-        gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
-        includepkgs => "libsmbios, smbios-utils, firmware-tools, firmware-addon-dell",
-        require => [File["/etc/pki/rpm-gpg/RPM-GPG-KEY-dell"], File["/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios"]]
-    }
+  # http://linux.dell.com/wiki/index.php/Repository/software
+  yumrepo {"dell-omsa-indep":
+    descr => "Dell OMSA repository - Hardware independent",
+    mirrorlist => "http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el\$releasever&basearch=\$basearch&dellsysidpluginver=\$dellsysidpluginver",
+    enabled => 1,
+    gpgcheck => 1,
+    gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
+    includepkgs => "libsmbios, smbios-utils, firmware-tools, firmware-addon-dell",
+    require => [File["/etc/pki/rpm-gpg/RPM-GPG-KEY-dell"], File["/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios"]]
+  }
 
-    file { "/etc/yum.repos.d/dell-software-repo.repo":
-        ensure => absent,
-    }
+  file { "/etc/yum.repos.d/dell-software-repo.repo":
+    ensure => absent,
+  }
 
 }
