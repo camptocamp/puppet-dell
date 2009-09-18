82a9e57df0d8e8f2ff64d49e488072d3ed9b43df
Marc Fournier <marc.fournier@camptocamp.com>
2009-09-18 15:22:16 +0200

    dell: another attempt to install openmanage without manually fixing stuff

--- manifests/classes/dell-openmanage.pp
+++ manifests/classes/dell-openmanage.pp
@@ -8,6 +8,8 @@ class dell::openmanage inherits dell::hwtools {
     require => Yumrepo["dell-omsa-indep"],
   }
 
+  # TODO: disable yum repositories une fois que OM est installé
+
   # Ces 2 repos hébergent openmanage, mais dépendent d'un plugin yum qui
   # va le analyser hardware et échoue si le système n'est pas supporté.
   #
@@ -17,53 +19,75 @@ class dell::openmanage inherits dell::hwtools {
   case $isopenmanagesupported {
     yes: {
 
+      # workaround broken yum repository
+      $url = "http://linux.dell.com/repo/hardware/latest/pe2970/rh${lsbmajdistrelease}0/srvadmin"
+      $ver = "6.1.0-648"
+
+      package { "srvadmin-omilcore":
+        ensure => present,
+        provider => "rpm",
+        source => "${url}/srvadmin-omilcore-${ver}.i386.rpm",
+        tag => "openmanage",
+        require => [Yumrepo["dell-omsa-specific"], Package["firmware-addon-dell"]],
+      }
+
+      package { "srvadmin-deng":
+        ensure => present,
+        provider => "rpm",
+        source => "${url}/srvadmin-deng-${ver}.i386.rpm",
+        tag => "openmanage",
+        require => [Package["srvadmin-omilcore"], Package["srvadmin-syscheck"]],
+      }
+
+      package { "srvadmin-omcommon":
+        ensure => present,
+        provider => "rpm",
+        source => "${url}/srvadmin-omcommon-${ver}.i386.rpm",
+        tag => "openmanage",
+        require => [Package["srvadmin-omilcore"], Package["srvadmin-syscheck"]],
+      }
+
+      package { ["srvadmin-hapi", "srvadmin-syscheck", "srvadmin-omauth"]:
+        ensure => present,
+        require => Package["srvadmin-omilcore"],
+      }
+
       package { ["srvadmin-storage", "srvadmin-omhip"]:
         ensure => present,
         tag => "openmanage",
         require => Package["srvadmin-omacore"],
       }
 
-      package { ["srvadmin-hapi", "srvadmin-syscheck", "srvadmin-omauth"]:
-        ensure => present,
-        tag => "openmanage",
-        require => Package["srvadmin-omilcore"],
-      }
-
-      package { ["srvadmin-cm"]:
+      package { "srvadmin-cm":
         ensure => present,
         tag => "openmanage",
         require => [Package["srvadmin-omacore"], Package["srvadmin-syscheck"]],
       }
 
-      package { ["srvadmin-isvc"]:
+      package { "srvadmin-isvc":
         ensure => present,
         tag => "openmanage",
-        require => [Package["srvadmin-hapi"], Package["srvadmin-deng"], Package["srvadmin-syscheck"]],
-      }
-
-      package { "srvadmin-deng":
-        ensure => present,
-        tag => "openmanage",
-        require => [Package["srvadmin-omilcore"], Package["srvadmin-syscheck"]],
+        require => [Package["srvadmin-hapi"], Package["srvadmin-deng"], Package["srvadmin-syscheck"], Package["srvadmin-omacore"]],
       }
 
       package { "srvadmin-omacore":
         ensure => present,
         tag => "openmanage",
-        require => [Package["srvadmin-deng"], Package["srvadmin-omilcore"]],
+        require => [Package["srvadmin-deng"], Package["srvadmin-omilcore"], Package["srvadmin-omcommon"]],
       }
 
-      package { "srvadmin-omilcore":
-        ensure => present,
-        tag => "openmanage",
-        require => [Yumrepo["dell-omsa-specific"], Package["firmware-addon-dell"]],
-      }
-
-      service{"dataeng":
+      service { "dataeng":
         ensure => running,
         tag => "openmanage",
         require => [Package["srvadmin-deng"], Package["srvadmin-storage"]],
       }
+
+      augeas { "disable dell yum plugin once OM is installed":
+        changes => "set /files/etc/yum/pluginconf.d/dellsysidplugin.conf/main/enabled 0",
+        require => Service["dataeng"],
+        notify  => Exec["update yum cache"],
+      }
+
     }
 
     no: {
