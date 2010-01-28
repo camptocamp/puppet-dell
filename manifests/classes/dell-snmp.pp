c98e58f5ee6bde5e776017029efd731d387b5f33
Cedric Jeanneret <cedric.jeanneret@camptocamp.com>
2010-01-28 12:28:52 +0100

    Correction and improvement
    - added missing notify on Service[snmpd]
    - added a check to ensure snmp is enabled for dataeng service.

--- manifests/classes/dell-snmp.pp
+++ manifests/classes/dell-snmp.pp
@@ -21,6 +21,13 @@ smuxpeer .1.3.6.1.4.1.674.10892.1
       augeas {"snmpd enable smux":
         context   => "/files/etc/default/snmpd/",
         changes   => "set SNMPDOPTS '\"-Lsd -u snmp -I smux -p /var/run/snmpd.pid -Lf /dev/null 127.0.0.1\"'",
+        notify    => Service["snmpd"],
+      }
+
+      exec {"activate omsa snmp":
+        command => "/etc/init.d/dataeng enablesnmp",
+        unless => "/etc/init.d/dataeng getsnmpstatus | grep -qi enabled",
+        notify => Service["dataeng"],
       }
     }
     default: {}
