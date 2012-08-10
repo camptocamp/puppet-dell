20cf50f145d845fd19c8fc57330e013586f3eb24
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2012-08-10 11:58:16 +0200

    move base concat for snmpd.conf from module dell to snmp

--- manifests/snmp.pp
+++ manifests/snmp.pp
@@ -6,14 +6,6 @@ Add a line to snmpd.conf which will publish Dell OMSA's infos through SNMP.
 */
 class dell::snmp {
 
-  include concat::setup
-
-  concat {'/etc/snmp/snmpd.conf':
-    owner  => 'root',
-    group  => 'root',
-    notify => [Service['snmpd'], Service['dataeng']],
-  }
-
   concat::fragment {'dell-omsa':
     target  => '/etc/snmp/snmpd.conf',
     content => "# section managed by puppet
@@ -21,6 +13,7 @@ class dell::snmp {
 # Allow Systems Management Data Engine SNMP to connect to snmpd using SMUX
 smuxpeer .1.3.6.1.4.1.674.10892.1
 ",
+    notify  => Service['dataeng'],
   }
 
   case $operatingsystem {
