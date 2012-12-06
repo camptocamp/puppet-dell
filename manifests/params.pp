5c57c20d02973e6adc055d55eae1f1a6f3abe659
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-06 17:22:40 +0100

    Lint dell::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,22 @@
 class dell::params {
 
   $omsa_url_base = $dell_omsa_url_base ? {
-    ""      => "http://linux.dell.com/repo/hardware/",
+    ''      => 'http://linux.dell.com/repo/hardware/',
     default => $dell_omsa_url_base,
   }
 
   $omsa_url_args_indep = $dell_omsa_url_args_indep ? {
-    ""      => 'osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver',
+    ''      => 'osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver',
     default => $dell_omsa_url_args_indep,
   }
 
   $omsa_url_args_specific = $dell_omsa_url_args_specific ? {
-    ""      => 'osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver',
+    ''      => 'osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver',
     default => $dell_omsa_url_args_specific,
   }
 
   $omsa_version = $dell_omsa_version ? {
-    ""      => "6.4",
+    ''      => '6.4',
     default => $dell_omsa_version,
   }
 
