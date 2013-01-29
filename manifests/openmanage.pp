26813d30b1b3b21a1e3d431efcd148b3bba3dbfe
Marc Remy <marc.remy@camptocamp.com>
2013-01-29 13:46:01 +0100

    Remove fact isopenmanagesupported

--- manifests/openmanage.pp
+++ manifests/openmanage.pp
@@ -2,26 +2,17 @@ class dell::openmanage {
 
   include dell::hwtools
 
-  service { 'dataeng': }
+  service { 'dataeng':
+    ensure    => running,
+    hasstatus => true,
+  }
 
-  # IMPORTANT: il faut tenir à jour la liste des systèmes supportés dans
-  # plugins/facter/isopenmanagesupported.rb
-  #
-  # Voir http://linux.dell.com/repo/hardware/latest
-  case $::isopenmanagesupported {
-    yes: {
-
-      Service['dataeng'] {
-        ensure    => running,
-        hasstatus => true,
-      }
-
-      file {'/etc/logrotate.d/openmanage':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        content => "# file managed by puppet
+  file {'/etc/logrotate.d/openmanage':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => "# file managed by puppet
 /var/log/TTY_*.log {
   missingok
   weekly
@@ -29,52 +20,45 @@ class dell::openmanage {
   compress
 }
 ",
-      }
-
-      file {'/etc/logrotate.d/perc5logs':
-        ensure  => absent,
-      }
-
-      tidy {'/var/log':
-        matches => 'TTY_*.log.*',
-        age     => '60d',
-        backup  => false,
-        recurse => true,
-      }
-
-      case $::operatingsystem {
-        /RedHat|CentOS/: {
-
-          # openmanage is a mess to install on redhat, and recent versions
-          # don't support older hardware. So puppet will install it if absent,
-          # or else leave it unmanaged.
-          include dell::openmanage::redhat
-
-          augeas { 'disable dell yum plugin once OM is installed':
-            changes => [
-              'set /files/etc/yum/pluginconf.d/dellsysidplugin.conf/main/enabled 0',
-              'set /files/etc/yum/pluginconf.d/dellsysid.conf/main/enabled 0',
-            ],
-            require => Service['dataeng'],
-          }
-
-        }
-
-        Debian: {
-          include dell::openmanage::debian
-        }
-
-        default: {
-          err("Unsupported operatingsystem: $::operatingsystem.")
-        }
-
-      }
-    }
-
-    no: {
-      exec{'unsupported openmanage warning':
-        command => "echo 'Either you have included this class on an unsupported machine (you shouldn\'t) or you haven\'t updated the list of supported systems in \$isopenmanagesupported.' && exit 1",
-      }
-    }
   }
+
+  file {'/etc/logrotate.d/perc5logs':
+    ensure  => absent,
+  }
+
+  tidy {'/var/log':
+    matches => 'TTY_*.log.*',
+    age     => '60d',
+    backup  => false,
+    recurse => true,
+  }
+
+  case $::operatingsystem {
+    /RedHat|CentOS/: {
+
+      # openmanage is a mess to install on redhat, and recent versions
+      # don't support older hardware. So puppet will install it if absent,
+      # or else leave it unmanaged.
+      include dell::openmanage::redhat
+
+      augeas { 'disable dell yum plugin once OM is installed':
+        changes => [
+          'set /files/etc/yum/pluginconf.d/dellsysidplugin.conf/main/enabled 0',
+          'set /files/etc/yum/pluginconf.d/dellsysid.conf/main/enabled 0',
+        ],
+        require => Service['dataeng'],
+      }
+
+    }
+
+    Debian: {
+      include dell::openmanage::debian
+    }
+
+    default: {
+      err("Unsupported operatingsystem: $::operatingsystem.")
+    }
+
+  }
+
 }
