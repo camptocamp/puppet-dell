fff03e60a483e0dfce3356c5fc1cc5e95f907098
Marc Remy <marc.remy@camptocamp.com>
2013-01-29 13:32:37 +0100

    Remove unused variable

--- manifests/hwtools.pp
+++ manifests/hwtools.pp
@@ -1,7 +1,6 @@
 class dell::hwtools {
 
   include dell::params
-  $ver = '6.3'
 
   # Dans ces paquets, on trouve de quoi flasher et extraires des infos des
   # bios & firmwares.
--- manifests/openmanage/redhat.pp
+++ manifests/openmanage/redhat.pp
@@ -1,7 +1,6 @@
 class dell::openmanage::redhat {
 
   include dell::params
-  $ver = '6.3'
 
   # this package contains the yum plugin which find the best yum repository
   # depending on the hardware.
