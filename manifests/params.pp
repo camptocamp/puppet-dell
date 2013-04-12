89b503c6b6df218036f277a7337c0209cf75d99c
Marc Remy <marc.remy@camptocamp.com>
2013-04-12 09:57:44 +0200

    Add OMSA version for R510

--- manifests/params.pp
+++ manifests/params.pp
@@ -46,6 +46,7 @@ class dell::params {
             'PowerEdge 1850' => 'OMSA_5.5',
             'PowerEdge 1950' => 'OMSA_6.1',
             'PowerEdge 2950' => 'OMSA_6.4',
+            'PowerEdge R510' => 'OMSA_6.4',
             'PowerEdge R610' => 'OMSA_6.4',
             'PowerEdge R620' => 'OMSA_7.2',
             default          => 'OMSA_5.4',
