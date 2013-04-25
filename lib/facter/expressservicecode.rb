7f0c0ba688698edb7c888a3efb7de92a5f719621
Marc Remy <marc.remy@camptocamp.com>
2013-04-25 17:18:42 +0200

    Fact for Express Service Code

---
+++ lib/facter/expressservicecode.rb
@@ -0,0 +1,14 @@
+if Facter.value(:id) == 'root' and
+   Facter.value(:is_virtual) == 'false' and
+   !Facter.value(:manufacturer).nil? and
+   Facter.value(:manufacturer).match(/dell/i)
+
+   if Facter.value(:serialnumber) and Facter.value(:serialnumber).length > 0
+
+      Facter.add('expressservicecode') do
+         setcode do
+            Facter.value(:serialnumber).to_i(36)
+         end
+      end
+   end
+end
