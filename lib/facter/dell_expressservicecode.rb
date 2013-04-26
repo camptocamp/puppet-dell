118a22d32fb37dcdcb3893d09fce892a229dcf30
Marc Remy <marc.remy@camptocamp.com>
2013-04-26 10:25:57 +0200

    rename fact to dell_expressservicecode

--- lib/facter/dell_expressservicecode.rb
+++ lib/facter/dell_expressservicecode.rb
@@ -5,7 +5,7 @@ if Facter.value(:id) == 'root' and
 
    if Facter.value(:serialnumber) and ( Facter.value(:serialnumber).length == 5 or Facter.value(:serialnumber).length == 7 )
 
-      Facter.add('expressservicecode') do
+      Facter.add('dell_expressservicecode') do
          setcode do
             Facter.value(:serialnumber).to_i(36)
          end
