c72970ee30e3647c6e3ba2aad2aa139c4d3085bd
Marc Remy <marc.remy@camptocamp.com>
2013-04-26 11:18:13 +0200

    use confine

--- lib/facter/dell_expressservicecode.rb
+++ lib/facter/dell_expressservicecode.rb
@@ -1,14 +1,11 @@
-if Facter.value(:id) == 'root' and
-   Facter.value(:is_virtual) == 'false' and
-   !Facter.value(:manufacturer).nil? and
-   Facter.value(:manufacturer).match(/dell/i)
-
-   if Facter.value(:serialnumber) and ( Facter.value(:serialnumber).length == 5 or Facter.value(:serialnumber).length == 7 )
-
-      Facter.add('dell_expressservicecode') do
-         setcode do
-            Facter.value(:serialnumber).to_i(36)
-         end
+Facter.add('dell_expressservicecode') do
+  confine :id => 'root'
+  confine :is_virtual => 'false'
+  if !Facter.value(:manufacturer).nil? and Facter.value(:manufacturer).match(/dell/i)
+    if Facter.value(:serialnumber) and ( Facter.value(:serialnumber).length == 5 or Facter.value(:serialnumber).length == 7 )
+      setcode do
+        Facter.value(:serialnumber).to_i(36)
       end
-   end
+    end
+  end
 end
