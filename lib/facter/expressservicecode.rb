952e5ed95effa333fbdc6c2245611bfe7c65e7e2
Marc Remy <marc.remy@camptocamp.com>
2013-04-26 08:31:02 +0200

    Service Tag seems to have 5 or 7 characters
    According to: http://linux.dell.com/libsmbios/main/SystemInfo_8h.html
    "const char * 	SMBIOSGetServiceTag ()
     	Returns the 5 or 7 character Dell Service Tag."

--- lib/facter/expressservicecode.rb
+++ lib/facter/expressservicecode.rb
@@ -3,7 +3,7 @@ if Facter.value(:id) == 'root' and
    !Facter.value(:manufacturer).nil? and
    Facter.value(:manufacturer).match(/dell/i)
 
-   if Facter.value(:serialnumber) and Facter.value(:serialnumber).length > 0
+   if Facter.value(:serialnumber) and ( Facter.value(:serialnumber).length == 5 or Facter.value(:serialnumber).length == 7 )
 
       Facter.add('expressservicecode') do
          setcode do
