df83111f024d68f554c56cb13522ef2f401a6be9
Marc Remy <marc.remy@camptocamp.com>
2015-02-17 16:15:59 +0100

    Allow to provide custom yum repo

--- manifests/openmanage/redhat.pp
+++ manifests/openmanage/redhat.pp
@@ -3,11 +3,18 @@
 #
 # Install openmanage tools on RedHat
 #
-class dell::openmanage::redhat {
+# $dell_repo: use the dell repo for yumrepo, or a already defined one.
+#  The yumrepo should have the name 'dell-omsa-specific'
+#
+class dell::openmanage::redhat(
+  $dell_repo = true,
+) {
 
   if (!defined(Class['dell'])) {
     fail 'You need to declare class dell'
   }
+
+  validate_bool( $dell_repo)
 
   # this package contains the yum plugin which find the best yum repository
   # depending on the hardware.
@@ -24,17 +31,19 @@ class dell::openmanage::redhat {
   # Ce repo héberge openmanage, mais dépendent d'un plugin yum qui
   # va analyser le hardware et échoue si le système n'est pas supporté.
   #
-  # http://linux.dell.com/repo/hardware/latest
-  yumrepo {'dell-omsa-specific':
-    descr      => 'Dell OMSA repository - Hardware specific',
-    mirrorlist => "${dell::omsa_url_base}${dell::omsa_version}/mirrors.cgi?${dell::omsa_url_args_specific}",
-    enabled    => 1,
-    gpgcheck   => 1,
-    gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
-    require    => [
-      File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
-      File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
-      ],
+  if $dell_repo {
+    # http://linux.dell.com/repo/hardware/latest
+    yumrepo {'dell-omsa-specific':
+      descr      => 'Dell OMSA repository - Hardware specific',
+      mirrorlist => "${dell::omsa_url_base}${dell::omsa_version}/mirrors.cgi?${dell::omsa_url_args_specific}",
+      enabled    => 1,
+      gpgcheck   => 1,
+      gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
+      require    => [
+        File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
+        File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
+        ],
+    }
   }
 
   # ensure file is managed in case we want to purge /etc/yum.repos.d/
