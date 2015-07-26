637294a914c5a121495cb38ff3d7813409fc07cd
gcmalloc <gcmalloc@gmail.com>
2015-07-26 17:56:28 +0200

    also fix the express tag

--- lib/facter/dell_express_service_code.rb
+++ lib/facter/dell_express_service_code.rb
@@ -1,6 +1,6 @@
 Facter.add('dell_express_service_code') do
   confine :id => 'root'
-  confine :is_virtual => 'false'
+  confine :is_virtual => [:false, 'false']
   if !Facter.value(:manufacturer).nil? and Facter.value(:manufacturer).match(/dell/i)
     if Facter.value(:serialnumber) and ( Facter.value(:serialnumber).length == 5 or Facter.value(:serialnumber).length == 7 )
       setcode do
