30257bfea447a123e6d47079fd669bbd13dfb966
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-10-02 10:08:39 +0200

    Add unit test for dell::openmanage

---
+++ spec/classes/dell__openmanage_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'dell::openmanage' do
+
+  let(:pre_condition) do
+    "class { '::dell': }"
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+    end
+  end
+end
+
