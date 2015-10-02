945b93205f67af757a7950e1e5a85e9c15f2c76c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-10-02 10:08:58 +0200

    Add acceptance test for dell::openmanage

---
+++ spec/acceptance/dell__openmanage_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper_acceptance'
+
+describe 'dell::openmanage' do
+
+  context 'when running puppet code' do
+    pp = "include ::dell::openmanage"
+
+    it 'should apply with no errors' do
+      apply_manifest(pp, :catch_failures => true)
+    end
+
+    it 'should converge on the first run' do
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+end
