af9b12304a065aa610172f86d1bfda156e21a224
Micha Krause <micha@noris.net>
2017-01-17 16:44:38 +0100

    added option to disable "include ::apt"

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
 class dell (
-  $omsa_url_base = $dell::params::omsa_url_base,
-  $omsa_url_args_indep = $dell::params::omsa_url_args_indep,
-  $omsa_url_args_specific = $dell::params::omsa_url_args_specific,
-  $omsa_version = $dell::params::omsa_version,
-  $customplugins = $dell::params::customplugins,
+  $omsa_url_base           = $dell::params::omsa_url_base,
+  $omsa_url_args_indep     = $dell::params::omsa_url_args_indep,
+  $omsa_url_args_specific  = $dell::params::omsa_url_args_specific,
+  $omsa_version            = $dell::params::omsa_version,
+  $customplugins           = $dell::params::customplugins,
   $check_warranty_revision = $dell::params::check_warranty_revision,
+  $manage_debian_apt       = $dell::params::manage_debian_apt,
 ) inherits ::dell::params {
 
   validate_string($omsa_url_base)
