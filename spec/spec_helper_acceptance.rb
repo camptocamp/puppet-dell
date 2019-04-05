f258cd7327984c68280eec1d8cd4c4af28bd8a40
Raphaël Pinson <raphael.pinson@camptocamp.com>
2019-04-05 16:52:54 +0200

    msync: test harness

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,30 +1,19 @@
-require 'beaker-rspec'
+require 'beaker-pe'
+require 'beaker-puppet'
+require 'puppet'
+require 'beaker-rspec/spec_helper'
+require 'beaker-rspec/helpers/serverspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+require 'beaker-task_helper'
 
-install_puppet_agent_on hosts, {}
-
-hosts.each do |host|
-  if fact('osfamily') == 'RedHat'
-    install_package host, 'epel-release'
-  end
-end
+run_puppet_install_helper
+configure_type_defaults_on(hosts)
+install_ca_certs unless pe_install?
+# install_bolt_on(hosts) unless pe_install?
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  module_name = module_root.split('-').last
-
-  # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module
-    puppet_module_install(:source => module_root, :module_name => module_name)
-
-    hosts.each do |host|
-      on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-vcsrepo'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-    end
-  end
 end
